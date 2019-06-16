bf7cdf6df0535522000d3101e8eb11df79926ecb
Luis Wirth <lwirth2000@gmail.com>
2019-06-16 23:11:00 +0200

    keras version implementiert; keras.py geloescht;kleine Schreibaenderungen

--- code/program/program.py
+++ code/program/program.py
@@ -1,6 +1,7 @@
 from utils import *
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers
 
 IMAGE_WIDTH = 28
 IMAGE_HEIGHT = 28
@@ -22,60 +23,45 @@ for img in imgs:
         for y in range(len(img[x])):
             img[x,y] /= 255.0
 
-print(imgs[0])
+print(imgs[0].shape)
 # imgs = loadGreyscaleImageDirectory('../data/trainingSample/0/')
 # plotImages(img)
 
+input_data = tf.keras.Input(shape=(IMAGE_WIDTH,IMAGE_HEIGHT)) #shape=(IMAGE_WIDTH,IMAGE_HEIGHT,IMAGE_DEPTH)
+econv0 = tf.keras.layers.Conv2D(filters=16,kernel_size=(3,3),strides=(1,1),padding='same',activation='relu')(input_data)
+maxpool0 = tf.keras.layers.MaxPooling2D(pool_size=(2,2),strides=None,padding='same')(econv0)
+econv1 = tf.keras.layers.Conv2D(filters=8,kernel_size=(3,3),strides=(1,1),padding='same',activation='relu')(maxpool0)
+maxpool1 = tf.keras.layers.MaxPooling2D(pool_size=(2,2),strides=None,padding='same')(econv1)
+econv2 = tf.keras.layers.Conv2D(filters=8,kernel_size=(3,3),strides=(1,1),padding='same',activation='relu')(maxpool1)
+maxpool2 = tf.keras.layers.MaxPooling2D(pool_size=(2,2),strides=None,padding='same')(econv2)
 
-learning_rate = 0.001
+encoded = maxpool2
 
-input_layer = tf.placeholder(tf.float32, (None, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS), name='inputs')
-target_layer = tf.placeholder(tf.float32, (None, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS), name='targets')
+dconv0 = tf.keras.layers.Conv2D(filters=8,kernel_size=(3,3),strides=(1,1),padding='same',activation='relu')(encoded)
+upsample0 = tf.keras.layers.UpSampling2D(size=(2,2),interpolation='nearest')(dconv0)
+dconv1 = tf.keras.layers.Conv2D(filters=8,kernel_size=(3,3),strides=(1,1),padding='same',activation='relu')(upsample0)
+upsample1 = tf.keras.layers.UpSampling2D(size=(2,2),interpolation='nearest')(dconv1)
+dconv2 = tf.keras.layers.Conv2D(filters=16,kernel_size=(3,3),strides=(1,1),padding='same',activation='relu')(upsample1)
+upsample2 = tf.keras.layers.UpSampling2D(size=(2,2),interpolation='nearest')(dconv2)
 
-""" Encoder """
-conv0 = tf.layers.conv2d(inputs=input_layer,filters=120,kernel_size=(3,3),padding='same',activation=tf.nn.relu)
-maxpool0 = tf.layers.max_pooling2d(inputs=conv0,pool_size=(2,2),strides=(2,2),padding='same')
-
-conv1 = tf.layers.conv2d(inputs=maxpool0,filters=160,kernel_size=(3,3),padding='same',activation=tf.nn.relu)
-maxpool1 = tf.layers.max_pooling2d(inputs=conv1,pool_size=(2,2),strides=(2,2),padding='same')
-
-conv2 = tf.layers.conv2d(inputs=maxpool1,filters=200,kernel_size=(3,3),padding='same',activation=tf.nn.relu)
-maxpool2 = tf.layers.max_pooling2d(inputs=conv2,pool_size=(2,2),strides=(2,2),padding='same')
-
-conv3 = tf.layers.conv2d(inputs=maxpool2,filters=240,kernel_size=(3,3),padding='same',activation=tf.nn.relu)
-maxpool3 = tf.layers.max_pooling2d(inputs=conv3,pool_size=(2,2),strides=(2,2),padding='same')
-
-encoded = maxpool3
-""" Decoder """
-upsample4 = tf.layers.resize_images(inputs=encoded,size=(8,8),method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-conv4 = tf.layers.conv2d(inputs=upsample4,filters=200,kernel_size=(3,3),padding='same',activation=tf.nn.relu)
-
-upsample5 = tf.layers.resize_images(inputs=conv4,size=(16,16),method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-conv5 = tf.layers.conv2d(inputs=upsample4,filters=160,kernel_size=(3,3),padding='same',activation=tf.nn.relu)
-
-upsample6 = tf.layers.resize_images(inputs=conv5,size=(32,32),method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-conv6 = tf.layers.conv2d(inputs=upsample4,filters=120,kernel_size=(3,3),padding='same',activation=tf.nn.relu)
-
-upsample7 = tf.layers.resize_images(inputs=conv6,size=(64,64),method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-conv7 = tf.layers.conv2d(inputs=upsample4,filters=15,kernel_size=(3,3),padding='same',activation=tf.nn.relu)
+decoded = upsample2
 
 
-logits = tf.layers.conv2d(inputs=conv7,filters=3,kernel_size=(3,3),padding='same',activation=None)
+autoencoder = tf.keras.Model(input_data,decoded)
+autoencoder.compile(optimizer='adadelta',loss='binary_crossentropy')
 
-decoded = tf.nn.sigmoid(logits)
+autoencoder.fit(x=imgs,y=imgs,batch_size=128,epochs=50,shuffle=True,validation_data=None,callbacks=[tf.keras.callbacks.TensorBoard(log_dir='/tmp/autoencoder')])
 
-loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=target_layer, logits=logits)
+generated_face = autoencoder.predict(imgs[0])
 
-cost = tf.reduce_mean(loss)
-opt = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+autoencoder.save('myautoencoder.model')
 
-sess - tf.Session()
-saver = tf.train.Saver()
-sess.run(tf.global_variables_initalizer())
-saver.restore(sess, "")
 
-for i, img in enumerate(imgs):
-    imageio = readImage(img)
-    imageio = imageio[:,:,0:3].reshape((1,64,64,3)) / 255.
-    file_name = os.path.basename(img)
-    denseRep = sess.run([encoded], feed_dict={inputs_layer: imageio, target_layer: imageio})
+# -----------------------------
+
+
+# for i, img in enumerate(imgs):
+#     imageio = readImage(img)
+#     imageio = imageio[:,:,0:3].reshape((1,64,64,3)) / 255.
+#     file_name = os.path.basename(img)
+#     denseRep = sess.run([encoded], feed_dict={inputs_layer: imageio, target_layer: imageio})
