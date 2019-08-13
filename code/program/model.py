1d3c40dba3feb0e8b5b4a6c10e4ff866b0ae4f64
Luis Wirth <lwirth2000@gmail.com>
2019-08-13 22:29:18 +0200

    Schreiben und programmieren und organisiert
    Schreiben: Literatur eingefuegt; Programmieren geschrieben
    Programmiert: verschiedenes

--- code/program/model.py
+++ code/program/model.py
@@ -3,21 +3,28 @@ import matplotlib.pyplot as plt
 
 import tensorflow as tf
 
+# hyperparameters
+BATCH_SIZE = 128
+EPOCHS = 100
+
+# load MNIST dataset
 (x_train, _), (x_test, _) = tf.keras.datasets.mnist.load_data()
+
+# format data
 x_train = x_train.astype('float32') / 255.
 x_test = np.reshape(x_test, (len(x_test),28,28,1))
 x_test = x_test.astype('float32') / 255.
 x_train = np.reshape(x_train, (len(x_train),28,28,1))
 
+# generate noise images / inputs
 noise_factor = 0.5
 x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
 x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape)
 x_train_noisy = np.clip(x_train_noisy, 0., 1.)
 x_test_noisy = np.clip(x_test_noisy, 0., 1.)
 
-
+# define model
 input_data = tf.keras.Input(shape=(28,28,1))
-
 econv0 = tf.keras.layers.Conv2D(filters=32, kernel_size=(3,3), strides=(1,1),padding='same',activation='relu')(input_data)
 emaxpool0 = tf.keras.layers.MaxPooling2D(pool_size=(2,2),strides=None,padding='same')(econv0)
 econv1 = tf.keras.layers.Conv2D(filters=32, kernel_size=(3,3), strides=(1,1),padding='same',activation='relu')(emaxpool0)
@@ -30,17 +37,20 @@ dupsample1 = tf.keras.layers.UpSampling2D(size=(2,2), interpolation='nearest')(d
 dconv2 = tf.keras.layers.Conv2D(filters=1,kernel_size=(3,3), strides=(1,1), padding='same', activation='sigmoid')(dupsample1)
 decoded = dconv2
 
+# compile model
 autoencoder = tf.keras.Model(input_data, decoded)
-autoencoder.compile(optimizer='adadelta',loss='binary_crossentropy')
+autoencoder.compile(optimizer='sgd',loss='binary_crossentropy')
 
 print(autoencoder.summary())
 
+# train model and save it
 autoencoder.fit(x=x_train_noisy, y=x_train, batch_size=128, epochs=100, shuffle=True,validation_data=(x_test_noisy,x_test),callbacks=[tf.keras.callbacks.TensorBoard(log_dir='/tmp/denoiser',histogram_freq=0,write_graph=False)])
-
-decoded_imgs = autoencoder.predict(x_test)
-
 autoencoder.save('denoiser.model')
 
+# let model make predictions
+decoded_imgs = autoencoder.predict(x_test)
+
+# show predictions
 n = 10
 plt.figure(figsize=(20,4))
 for i in range(n):
--- notes.md
+++ notes.md
@@ -1,13 +1,21 @@
 # Planen
 
+## Gloor Empfehlungen
+
+* Eigenleistungen hervorheben (nicht nur Theorie)
+* Vorwort und Einleitung schreiben
+* Im Vorwort auf Leitfragenaenderung eingehen
+* Relevanz der erklaerten Theorie besser in Text einarbeiten
+* Eigenstaendige Gedanken / Programmbeispiele in den Vordergrund
+
+* Padding und Stride kuerzen
+* ReLu erklaeren
+
 ## Schreiben
-* Visualisierung des Lernvorganges -> [tf.playground](http://playground.tensorflow.org/)
 * Relevanz von Data-shuffling erklaeren (Modell wird nicht durch Reihenfolge der Daten missgeleitet)
 * Relevanz von Data-normalization (wegen Aktiverungsfunktion -> Sigmoid kann nur 0 bis 1 und auch sonst vorteilhafter)
 
 ## Programmieren
-* Face landmark detection
-* Face aligner -> [OpenCV-Tut](https://docs.opencv.org/2.4/modules/contrib/doc/facerec/tutorial/facerec_gender_classification.html)
 
 # Erkenntnisse
 
