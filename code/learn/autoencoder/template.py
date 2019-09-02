ccff81d73d93b95f28a9e47e342f4990f82398d0
Luis Wirth <lwirth2000@gmail.com>
2019-09-02 20:24:12 +0200

    code/learn gelöscht

--- code/learn/autoencoder/template.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import  Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Conv2D
-
-#tensorboard = TensorBoard(log_dir='logs/')
-
-#model.fit(callbacks=[tensorboard])
--- code/learn/python/utiltest.py
+++
@@ -1,4 +0,0 @@
-#this is a module
-
-def testFun():
-    print('Iam a funciton from another module')
