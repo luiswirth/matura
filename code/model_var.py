fc07f63cc6d8c054d124ee85ea93390cf448b3ea
Luis Wirth <lwirth2000@gmail.com>
2019-09-07 15:55:29 +0200

    Bilder; Code

--- code/model_var.py
+++ code/model_var.py
@@ -3,55 +3,55 @@ import matplotlib.pyplot as plt
 
 import tensorflow as tf
 
-# hyperparameters
+# zu testende Hyperparameter
 EPOCHS = 20
 
-POSSIBLE_BATCH_SIZES = [ 16, 32, 64, 128, 256]
-POSSIBLE_NUMS_FILTER = [ 16, 32, 64, 128, 256]
-POSSIBLE_ACITVATIONFUNCTIONS = [ 'sigmoid', 'relu' ]
+BATCH_SIZES = [ 16, 32, 64, 128, 256 ]
+FILTER_NUMS = [ 16, 32, 64, 128, 256 ]
+ACTIVATIONFUNCTIONS = [ 'sigmoid', 'relu' ]
 
-
-# load MNIST dataset
+# MNIST-Datensatz laden
 (x_train, _), (x_test, _) = tf.keras.datasets.mnist.load_data()
 
-# format data
-x_train = x_train.astype('float32') / 255.
-x_test = np.reshape(x_test, (len(x_test),28,28,1))
-x_test = x_test.astype('float32') / 255.
-x_train = np.reshape(x_train, (len(x_train),28,28,1))
+# Daten formatieren
+x_train = x_train.astype('float32') / 255.0
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
+x_test = x_test.astype('float32') / 255.0
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
 
-# generate noise images / inputs
+# verrauschte Bilder (Input) generieren
 noise_factor = 0.5
 x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
 x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape)
-x_train_noisy = np.clip(x_train_noisy, 0., 1.)
-x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+x_train_noisy = np.clip(x_train_noisy, 0.0, 1.0)
+x_test_noisy = np.clip(x_test_noisy, 0.0, 1.0)
 
-# test different models
-for activation_function in POSSIBLE_ACITVATIONFUNCTIONS:
-    for batch_size in POSSIBLE_BATCH_SIZES:
-        for num_filters in POSSIBLE_NUMS_FILTER:
+# verschiedene Modelle testen
+for activation_function in ACTIVATIONFUNCTIONS:
+    for batch_size in BATCH_SIZES:
+        for num_filters in FILTER_NUMS:
 
             input_data = tf.keras.Input(shape=(28,28,1))
-            econv0 = tf.keras.layers.Conv2D(filters=num_filters, kernel_size=(3,3), strides=(1,1),padding='same',activation=activation_function)(input_data)
+            econv0 = tf.keras.layers.Conv2D(filters=num_filters, kernel_size=(3,3), strides=(1,1),padding='same',activation=activation_function, kernel_initializer='glorot_normal')(input_data)
             emaxpool0 = tf.keras.layers.MaxPooling2D(pool_size=(2,2),strides=None,padding='same')(econv0)
-            econv1 = tf.keras.layers.Conv2D(filters=num_filters, kernel_size=(3,3), strides=(1,1),padding='same',activation=activation_function)(emaxpool0)
+            econv1 = tf.keras.layers.Conv2D(filters=num_filters, kernel_size=(3,3), strides=(1,1),padding='same',activation=activation_function, kernel_initializer='glorot_normal')(emaxpool0)
             emaxpool1 = tf.keras.layers.MaxPooling2D(pool_size=(2,2), strides=None,padding='same')(econv1)
             encoded =  emaxpool1 # bottleneck (shape=(7,7,32))
-            dconv0 = tf.keras.layers.Conv2D(filters=num_filters, kernel_size=(3,3), strides=(1,1), padding='same', activation=activation_function)(encoded)
+            dconv0 = tf.keras.layers.Conv2D(filters=num_filters, kernel_size=(3,3), strides=(1,1), padding='same', activation=activation_function, kernel_initializer='glorot_normal')(encoded)
             dupsample0 = tf.keras.layers.UpSampling2D(size=(2,2), interpolation='nearest')(dconv0)
-            dconv1 = tf.keras.layers.Conv2D(filters=num_filters, kernel_size=(3,3), strides=(1,1), padding='same', activation=activation_function)(dupsample0)
+            dconv1 = tf.keras.layers.Conv2D(filters=num_filters, kernel_size=(3,3), strides=(1,1), padding='same', activation=activation_function, kernel_initializer='glorot_normal')(dupsample0)
             dupsample1 = tf.keras.layers.UpSampling2D(size=(2,2), interpolation='nearest')(dconv1)
-            dconv2 = tf.keras.layers.Conv2D(filters=1,kernel_size=(3,3), strides=(1,1), padding='same', activation='sigmoid')(dupsample1)
+            dconv2 = tf.keras.layers.Conv2D(filters=1,kernel_size=(3,3), strides=(1,1), padding='same', activation='sigmoid', kernel_initializer='glorot_normal')(dupsample1)
             decoded = dconv2
 
-            # compile model
+            # Modell kompilieren
             autoencoder = tf.keras.Model(input_data, decoded)
-            autoencoder.compile(optimizer='sgd',loss='binary_crossentropy', metrics=['accuracy'])
+            autoencoder.compile(optimizer='sgd', loss='mean_squared_error', metrics=['accuracy'])
 
             print(autoencoder.summary())
 
-            # train model and save it
-            NAME = '{}-{}-batch-{}-filters-{}-denoiser'.format(loss, activation_function, batch_size, num_filters)
+            # Training und TensorBoard
+            NAME = 'denoiser-{}-{}-batches-{}-filters'.format(activation_function, batch_size, num_filters)
             tensorboard = tf.keras.callbacks.TensorBoard(log_dir='logs/{}'.format(NAME))
-            autoencoder.fit(x=x_train_noisy, y=x_train, batch_size=batch_size, epochs=EPOCHS, shuffle=True,validation_data=(x_test_noisy,x_test),callbacks=[tensorboard])
+            autoencoder.fit(x=x_train_noisy, y=x_train, batch_size=batch_size, epochs=EPOCHS, shuffle=True, validation_data=(x_test_noisy,x_test), callbacks=[tensorboard])
+            autoencoder.save('models/{}', NAME)
--- code/test.py
+++ code/test.py
@@ -3,40 +3,24 @@ import matplotlib.pyplot as plt
 
 import tensorflow as tf
 
+# MNIST-Datensatz laden
 (x_train, _), (x_test, _) = tf.keras.datasets.mnist.load_data()
-x_train = x_train.astype('float32') / 255.
-x_test = np.reshape(x_test, (len(x_test),28,28,1))
-x_test = x_test.astype('float32') / 255.
-x_train = np.reshape(x_train, (len(x_train),28,28,1))
 
+# Daten formatieren
+x_train = x_train.astype('float32') / 255.0
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
+x_test = x_test.astype('float32') / 255.0
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
+
+# verrauschte Bilder (Input) generieren
 noise_factor = 0.5
-x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
+
+# für x_train
+noise_matrices = np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
+noise_matrices *= noise_factor
+x_train_noisy = x_train + noise_matrices
+x_train_noisy = np.clip(x_train_noisy, 0.0, 1.0)
+
+# für x_test in Kurzfassung
 x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape)
-x_train_noisy = np.clip(x_train_noisy, 0., 1.)
-x_test_noisy = np.clip(x_test_noisy, 0., 1.)
-
-autoencoder = tf.keras.models.load_model('denoiser.model') # already compiled
-
-print(autoencoder.summary())
-
-decoded_imgs = autoencoder.predict(x_test)
-
-n = 10 # jeweils 10 Bilder
-plt.figure()
-for i in range(n):
-    # verrauschte Bilder
-    ax = plt.subplot(3, n, i+1)
-    plt.imshow(x_test_noisy[i].reshape(28,28))
-    plt.gray()
-
-    # entrauschte Bilder
-    ax = plt.subplot(3, n, i+1+n)
-    plt.imshow(decoded_imgs[i].reshape(28,28))
-    plt.gray()
-
-    # Original-Bilder
-    ax = plt.subplot(3, n, i+1+2*n)
-    plt.imshow(x_test[i].reshape(28,28))
-    plt.gray()
-
-plt.show()
+x_test_noisy = np.clip(x_test_noisy, 0.0, 1.0)
